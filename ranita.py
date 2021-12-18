6d59d0fe7a5576624b8567480d9cbf77acfe32b7
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-18 17:04:40 +0100

    commit1

--- ranita.py
+++ ranita.py
@@ -1,5 +1,51 @@
+mapa= [
+    ["X" , "I" , "X" , "X" , "X" , "X" , "X"],
+    ["X" , " " , "X" , "X" , "T1" , " " , "X"],
+    ["X" , " " , "T2" , "X" , " " , " " , "X"],
+    ["X" , " " , " " , "X" , "B" , " " , "X"],
+    ["X" , " " , " " , "X" , "T2" , " " , "X"],
+    ["X" , "T1" , "B" , "X" , "X" , " " , "X"],
+    ["X" , "X" , "X" , "X" , "X" , "S" , "X"],
+]
+
+x = 0
+y = 0
+
+print("La rana empieza en la posición (0,0)")
+print((mapa[y])[x])
+i = 0
+
+while (mapa[y])[x] == " ":
+    direccion = input("Selecciona la dirección en la que deseas moverte (ARRIBA, ABAJO, DERECHA, IZQUIERDA): ")
+    
+    if direccion == "ABAJO" or "ARRIBA" or "DERECHA" or "IZQUIERDA":
+        print("Has seleccionado {}".format(direccion))
+    
+    if direccion == "ABAJO":
+        x = x
+        y = y + 1
+    elif direccion == "ARRIBA":
+        x = x
+        y = y - 1
+    elif direccion == "DERECHA":
+        x = x - 1
+        y = y 
+    elif direccion == "IZQUIERDA":
+        x = x + 1
+        y = y 
+    else:
+        print("SOLO SE PUEDEN ELEGIR LAS OPCIONES DE DIRECCIÓN EXPLICADAS, ELIGE BIEN")
+        print("\n")
 
 
+    print("Estás en la casilla ({},{})".format(x,y))
+    if (mapa[y])[x] == ' ':
+        print("La casilla elegida NO es un muro, puede continuar en el laberinto")
+    elif (mapa[y])[x] == 'S':
+        print("Has llegado a la salida del laberibnto. ¡HAS GANADO!")
+        break
+    else:
+        print("La casilla es un muro \nReinicia el programa para seguir jugando")
 
 #if __name__ == '__main__':
     #first_multiple_input = input().rstrip().split()
