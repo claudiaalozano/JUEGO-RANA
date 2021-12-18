1c65dd457e1b384d4f68ef99ab14bb6f251ef606
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-18 16:29:03 +0100

    emepezamos desde el principio

--- ranita.py
+++ ranita.py
@@ -1,25 +1,11 @@
-import math
-import os
-import random
-import re
-import sys
 
-class coordenadas:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-    def comparate(self , x , y):
-        if(self.x==x and self.y==y):
-            return True
-        else:
-            return False
 
 
 #if __name__ == '__main__':
-#first_multiple_input = input().rstrip().split()
-#n = int(first_multiple_input[0])
-#m = int(first_multiple_input[1])
-#k = int(first_multiple_input[2])
+    #first_multiple_input = input().rstrip().split()
+    #n = int(first_multiple_input[0])
+    #m = int(first_multiple_input[1])
+    #k = int(first_multiple_input[2])
 
 #for n in range(n):
     #row = input()
