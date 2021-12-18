462c2be554c10bf8b503bd061ce1d2e15735fab0
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-18 17:49:52 +0100

    Ultimos cambios

--- ranita.py
+++ ranita.py
@@ -67,20 +67,3 @@ while (mapa[y])[x] != "X":
             print("Has encontrado un tunel, la rana se mueve a la casilla (5,4)") 
     else:
         print("La casilla es un muro \nReinicia el programa para seguir jugando")
-
-#if __name__ == '__main__':
-    #first_multiple_input = input().rstrip().split()
-    #n = int(first_multiple_input[0])
-    #m = int(first_multiple_input[1])
-    #k = int(first_multiple_input[2])
-
-#for n in range(n):
-    #row = input()
-
-
-#for k_itr in range(k):
-    #second_multiple_input = input().rstrip().split()
-    #i1 = int(second_multiple_input[0])
-    #j1 = int(second_multiple_input[1])
-    #i2 = int(second_multiple_input[2])
-    #j2 = int(second_multiple_input[3])
