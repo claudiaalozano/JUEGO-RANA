e3dda18322fcc8fef202c31582eb71d8a9013a08
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-18 16:24:09 +0100

    commit

--- ranita.py
+++ ranita.py
@@ -4,21 +4,30 @@ import random
 import re
 import sys
 
-if __name__ == '__main__':
-    first_multiple_input = input().rstrip().split()
-    n = int(first_multiple_input[0])
-    m = int(first_multiple_input[1])
-    k = int(first_multiple_input[2])
+class coordenadas:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def comparate(self , x , y):
+        if(self.x==x and self.y==y):
+            return True
+        else:
+            return False
 
-for n_itr in range(n):
-    row = input()
-# Write your code here
 
-for k_itr in range(k):
-    second_multiple_input = input().rstrip().split()
-    i1 = int(second_multiple_input[0])
-    j1 = int(second_multiple_input[1])
-    i2 = int(second_multiple_input[2])
-    j2 = int(second_multiple_input[3])
-# Write your code here
-# Write your code here
+#if __name__ == '__main__':
+#first_multiple_input = input().rstrip().split()
+#n = int(first_multiple_input[0])
+#m = int(first_multiple_input[1])
+#k = int(first_multiple_input[2])
+
+#for n in range(n):
+    #row = input()
+
+
+#for k_itr in range(k):
+    #second_multiple_input = input().rstrip().split()
+    #i1 = int(second_multiple_input[0])
+    #j1 = int(second_multiple_input[1])
+    #i2 = int(second_multiple_input[2])
+    #j2 = int(second_multiple_input[3])
